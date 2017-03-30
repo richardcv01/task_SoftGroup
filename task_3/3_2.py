f80403d33d5ab2fefcfa7ed1ab4eacf5287ceafb
admin24 <richardcv@ukr.net>
2017-03-30 10:26:37 +0300

    add coment task 2_1

--- task_3/3_2.py
+++ task_3/3_2.py
@@ -4,35 +4,24 @@ import json
 import collections as col
 
 def csv_load(file: object) -> str:
-    reader = csv.reader(file)
+    reader = csv.reader(file, delimiter=';')
     stRes = ''
-    listRes = []
     for row in reader:
-        row = ''.join(row).split(';')
-        stRes = stRes + ' '.join(row)
-        listRes.append(' '.join(row))
-    count = len(listRes)
-    i = 0
-    for i in range(0, count-1):
-        listRes[i] = listRes[i] + '\n'
-    stRes = ''.join(listRes)
+        stRes += ' '.join(row) + '\n'
+    stRes = stRes.rstrip('\n')
+    print(r'' + stRes)
     return stRes
 
-
 def csv_save(s: str, file: object) -> None:
-    Ls_line = s.split('\n')
-    LsRes = [s.replace(' ', ';') for s in Ls_line]
-    StRes = '\n'.join(LsRes).replace("'", '"')
+    StRes = s.replace(' ', ';').replace("'", '"')
+    print(StRes)
     file.write(StRes)
 
 def json_load(file: object) -> str:
     dicOrd = json.load(file, object_pairs_hook=col.OrderedDict)
-    stRes = ""
     listRes = dicOrd["rows"]
-    count = len(listRes)
     stRes = "\n".join(listRes)
     return stRes
-
 
 
 def json_save(s: str, file: object) -> None:
@@ -40,6 +29,9 @@ def json_save(s: str, file: object) -> None:
     DicOrder = col.OrderedDict()
     DicOrder["rows"] = St
     file.write(str(dict(DicOrder)).replace("'", '"'))
+
+with open('json.txt', 'w') as file:
+   json_save('Gaius Julius Caesar 55\nMarcus Aurelius Antoninus 58',file)
 
 
 class AbsConverterFabric(ABC):
@@ -58,19 +50,21 @@ class AbstractConverter(ABC):
         raise NotImplemented
 
 
+class Converter(AbstractConverter):
+    pass
+
 class ConverterFabric(AbsConverterFabric):
     dict_load = {'csv': csv_load, 'json':json_load}
     dict_save = {'csv': csv_save, 'json':json_save}
-    class Converter(AbstractConverter):
-        pass
+
 
     def create_converter(self, _from: str, _to: str) -> object:
         self._from = _from
         self._to = _to
-        ConverterFabric.Converter = type('Converter', (),
+        Converter = type('Converter', (),
                                          {'load':ConverterFabric.dict_load[self._from]
             , 'save':ConverterFabric.dict_save[self._to]})
-        obj = ConverterFabric.Converter
+        obj = Converter
         return obj
 
 
@@ -81,20 +75,20 @@ if __name__ == '__main__':
     converter1 = fab.create_converter('csv', 'json')
     converter2 = fab.create_converter('json', 'csv')
 
-    with open('csv.txt', 'r') as file:
-        result = converter1.load(file)
-        print(result,'1')
-    print()
+    #with open('csv.txt', 'r') as file:
+    #    result = converter1.load(file)
+   #     print(result,'1')
+  #  print()
 
 
-    with open('json.txt', 'w') as file:
-        converter1.save(result, file)
+ #   with open('json.txt', 'w') as file:
+#        converter1.save(result, file)
 
-    with open('json.txt', 'r') as file:
-        result = converter2.load(file)
-        print(result, '2')
+    #with open('json.txt', 'r') as file:
+   #     result = converter2.load(file)
+  #      print(result, '2')
 
-    with open('csv.txt', 'w') as file:
-        converter2.save(result, file)
+#    with open('csv.txt', 'w') as file:
+ #       converter2.save(result, file)
 
 
