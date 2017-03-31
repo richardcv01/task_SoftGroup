284ff4470edf558444ec5797de88e6f3dafd29e2
admin24 <richardcv@ukr.net>
2017-03-31 16:11:43 +0300

    add coment task 2_1

---
+++ task_4/4_1.py
@@ -0,0 +1,32 @@
+import time
+import random
+from threading import Thread, Lock, RLock
+import threading
+
+LOCK = threading.RLock()
+
+
+def worker(file: object,  *args):
+    """
+    worker function writes two strings with random pause to
+    shared file using synchronization primitive
+    :param file: file-object
+    :return:
+    """
+    global LOCK
+    LOCK.acquire()
+    file.write( args[0] +  ': ' + 'started.\n')
+    time.sleep(random.random() * 5)
+    file.write( args[0] +': ' + 'done.\n')
+    LOCK.release()
+
+
+if __name__ == '__main__':
+    file = open('test.txt', 'a', encoding='utf-8')
+    for i in range(1, 11):
+        name = 'thread_{}'.format(i)
+        t = threading.Thread(target=worker, name=name, args=(file,name))
+        t.start()
+
+
+
