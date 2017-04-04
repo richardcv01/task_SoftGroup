0d6bae8b74b098a3cf5633ef1f39bdcdc084720f
admin24 <richardcv@ukr.net>
2017-04-04 16:13:05 +0300

    add coment task 2_1

---
+++ task_5/test_chat/client.py
@@ -0,0 +1,20 @@
+import socket
+import time
+
+host = "127.0.0.1"
+port = 8080
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port))
+print(s.recv(1024).decode('utf8'))
+
+while True:
+    buf = input()
+    s.send(buf.encode('utf8'))
+    result = s.recv(1024)
+    print('Ответ сервера: ', result.decode('utf8'))
+    if buf == "exit":
+        break
+s.close()
+
+time.sleep(10)
