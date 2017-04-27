8137c5c3cfcfbdb75f8414eeae2fdeb2e994795e
admin24 <richardcv@ukr.net>
2017-04-27 17:57:01 +0300

    07.04.2017 in work

--- task_7/flaskr/flaskr.py
+++ task_7/flaskr/flaskr.py
@@ -3,13 +3,6 @@ import os
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 
-# конфигурация
-DATABASE = 'flaskr.db'
-DEBUG = True
-SECRET_KEY = 'development key'
-USERNAME = 'admin'
-PASSWORD = 'default'
-
 app = Flask(__name__)
 app.config.from_object(__name__)
 
